9d078ca05705d4a64e465dd0f85b7553371978c0
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-02-27 09:55:51 +0100

    Prevent potential spamming of the Zulip API
    By storing last status we can avoid setting it repeatedly to the same
    status

--- set_status.sh
+++ set_status.sh
@@ -14,10 +14,14 @@ SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport 
 SSID=$(echo -e "$SSID" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 TIMESTAMP=$(date '+%d-%m-%Y %H:%M:%S')
 CURRENT_DAY=$(date '+%A')
+LAST_STATUS=`head -n 1 /tmp/zulip_status 2> /dev/null`;
+LAST_STATUS=${LAST_STATUS:=none}
 
 echo "$TIMESTAMP"
 
 if [[ "$CURRENT_DAY" == "Saturday"  ||  "$CURRENT_DAY" == "Sunday" ]]; then
+    if [ "$LAST_STATUS" == "weekend" ]; then exit 0; fi
+
     echo "Setting Zulip status to weekend time";
     curl -sSX POST $ZULIP_DOMAIN/api/v1/users/me/status \
     -u $ZULIP_USERNAME:$ZULIP_API_KEY \
@@ -25,12 +29,15 @@ if [[ "$CURRENT_DAY" == "Saturday"  ||  "$CURRENT_DAY" == "Sunday" ]]; then
     --data-urlencode away=true \
     --data-urlencode emoji_name=cocktail \
     --data-urlencode reaction_type=unicode_emoji
+    echo "weekend" > /tmp/zulip_status
     echo "---------------------------"
     exit 0
 fi
 
 
 if [ "$SSID" == "$SSID_HOME" ]; then
+    if [ "$LAST_STATUS" == "home" ]; then exit 0; fi
+
     echo "Setting Zulip status to: Arbejder hjemmefra";
     curl -sSX POST $ZULIP_DOMAIN/api/v1/users/me/status \
     -u $ZULIP_USERNAME:$ZULIP_API_KEY \
@@ -38,7 +45,11 @@ if [ "$SSID" == "$SSID_HOME" ]; then
     --data-urlencode away=false \
     --data-urlencode emoji_name=house \
     --data-urlencode reaction_type=unicode_emoji
+
+    echo "home" > /tmp/zulip_status
 elif [ "$SSID" == "$SSID_OFFICE" ]; then
+    if [ "$LAST_STATUS" == "office" ]; then exit 0; fi
+
     echo "Setting Zulip status to: På Suomisvej";
     curl -sSX POST $ZULIP_DOMAIN/api/v1/users/me/status \
     -u $ZULIP_USERNAME:$ZULIP_API_KEY \
@@ -46,7 +57,11 @@ elif [ "$SSID" == "$SSID_OFFICE" ]; then
     --data-urlencode away=false \
     --data-urlencode emoji_name=office \
     --data-urlencode reaction_type=unicode_emoji
+
+    echo "office" > /tmp/zulip_status
 else
+    if [ "$LAST_STATUS" == "remote" ]; then exit 0; fi
+
     echo "Setting Zulip status to: Arbejder remote";
     curl -sSX POST $ZULIP_DOMAIN/api/v1/users/me/status \
     -u $ZULIP_USERNAME:$ZULIP_API_KEY \
@@ -54,6 +69,8 @@ else
     --data-urlencode away=false \
     --data-urlencode emoji_name=house \
     --data-urlencode reaction_type=unicode_emoji
+
+    echo "remote" > /tmp/zulip_status
 fi
 
 echo "---------------------------"
